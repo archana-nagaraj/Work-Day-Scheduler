b28e2c850a9d5e1fffe9c193aca2cbe6003a7fa3
Archana Nagaraja <johndoe@example.com>
2020-12-28 21:03:53 -0800

    reworked on timeblocks- classes and ids to match the given css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,13 +2,7 @@
 const today = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").append(today);
 
-// add timeblocks 9 AM to 5 PM - a total of 9 blocks
-// Create a row with three colums - (small-large-small)
 
-// using jquery add the row with three columns and append to the container in index.html
-// create the loop to display such rows 9 times
-// add styling
-// add text in the timeblocks
 
 
    
