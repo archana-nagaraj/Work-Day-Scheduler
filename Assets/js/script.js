e8be3e294ed69cefbe2ef225d09b26beeccce4b5
Archana Nagaraja <johndoe@example.com>
2021-01-02 08:46:06 -0800

    added hourcheck function which compares current hour with the block hour and updates the css styling based onpast/present and future classes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,53 @@
 // display current day in jumbotron
-const today = moment().format('dddd, MMMM Do YYYY');
+const today = moment().format('dddd, MMMM Do YYYY'); // using moment.js
 $("#currentDay").append(today);
 
-
-//localStorage.clear(); // Remove all local storage items if any 
-
-// making sure the DOM is ready
-$( document ).ready(function() {
+$( document ).ready(function() {  //making sure the DOM is ready - runs HTML and CSS first
     console.log( "ready!" );
     $(".saveBtn").on("click", function(){
-        //using jQuery Traversing
+    //using jQuery Traversing
     var text = $(this).siblings(".description").val(); //Returns the value of sibling of saveButton with class .description
-    console.log(text);
     var time = $(this).parent().attr("id"); //Returns the direct parent element of the saveButton
-    console.log(time);
-     // Store event time and text in local storage 
-     localStorage.setItem(time, text);
+    // Store event time and text in local storage 
+    localStorage.setItem(time, text);
     });
 });
 
   // Get event time and text from local storage 
-$("#9AM .description").val(localStorage.getItem("9AM"));
-$("#10AM .description").val(localStorage.getItem("10AM"));
-$("#11AM .description").val(localStorage.getItem("11AM"));
-$("#12PM .description").val(localStorage.getItem("12PM"));
-$("#1PM .description").val(localStorage.getItem("1PM"));
-$("#2PM .description").val(localStorage.getItem("2PM"));
-$("#3PM .description").val(localStorage.getItem("3PM"));
-$("#4PM .description").val(localStorage.getItem("4PM"));
-$("#5PM .description").val(localStorage.getItem("5PM"));
+  $("#9AM .description").val(localStorage.getItem("9AM"));
+  $("#10AM .description").val(localStorage.getItem("10AM"));
+  $("#11AM .description").val(localStorage.getItem("11AM"));
+  $("#12PM .description").val(localStorage.getItem("12PM"));
+  $("#1PM .description").val(localStorage.getItem("1PM"));
+  $("#2PM .description").val(localStorage.getItem("2PM"));
+  $("#3PM .description").val(localStorage.getItem("3PM"));
+  $("#4PM .description").val(localStorage.getItem("4PM"));
+  $("#5PM .description").val(localStorage.getItem("5PM"));
 
 
- 
+function hourCheck() {
+    // get the current hout using moment.js
+    var currentHour = moment().hours();
+    console.log(currentHour);
 
+    // iterate over each hour in the timeblock and compare it with the current hour
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockHour);
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+          } 
+          else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+          } 
+          else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+          }
+    });
+}
+
+hourCheck();
 
